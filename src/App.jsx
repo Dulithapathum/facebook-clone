37ee73065400bdf88b9990d9a08aa712c8524a16
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2024-02-10 16:50:27 +0530

    Basic files Created

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Home from "./pages/Home/Home";
+function App() {
+  return <div className="App">
+  
+  </div>;
+}
+
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import NavBar from "../../components/Navigation/NavBar";
+
+export default function Home() {
+  return <>
+  </>;
+}
